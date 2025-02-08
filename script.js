c9b466fa6c587cc32969ad70c2bc6297753693e3
Shashank Raj <shashankraj72777@gmail.com>
2025-02-08 10:26:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
